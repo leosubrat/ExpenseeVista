c1a99011b960db8883d4dcc110943077b7027161
Subrat Thapa <subratthapa2018@gmail.com>
2025-01-16 08:49:02 +0545

    Creating empty MAUI Blazor Hybrid App

---
+++ ExpenseeVista/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseeVista
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ ExpenseeVista/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExpenseeVista
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
